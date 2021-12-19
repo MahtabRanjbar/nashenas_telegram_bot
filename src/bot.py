70dcb0e16a8c26f4e8d734024a2c6cee23c6ab2b
MahtabRanjbar <mahtabranjbar93@gmail.com>
2021-12-20 00:43:35 +0330

    update telegram design for handlers & add admin filter

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,8 @@
 import os
 
-import emoji
 import telebot
-from loguru import logger
 
-from src.utils.constants import keyboards
-from src.utils.io import write_json
-
-
-class Bot:
-    """
-    Template for telegram Bot.
-    """
-    def __init__(self):
-        self.bot = telebot.TeleBot(os.environ['NASHENAS_BOT_TOKEN'])
-        self.echo_all = self.bot.message_handler(func=lambda message:True)(self.echo_all)
-        
-    def run(self):
-        logger.info('Bot is running...')
-        self.bot.infinity_polling()
-        
-    def echo_all(self,message):
-        #write_json(message.json, 'messages.json' )
-        self.bot.reply_to(message, message.text)
-        self.bot.send_message(message.chat.id, message.text, reply_markup=keyboards.main)
-        
-
-
-if __name__ == '__main__':
-    logger.info('Bot statred')
-    bot = Bot()
-    bot.run()
+# Initialize bot
+bot = telebot.TeleBot(
+    os.environ['NASHENAS_BOT_TOKEN'], parse_mode='HTML'
+)
